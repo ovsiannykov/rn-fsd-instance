779ce62b761282d44042f4c350fc1c4f1f8bb6d1
ovsiannykov.dev@gmail.com <ovsiannykov.dev@gmail.com>
2023-09-28 16:09:12 +0300

    test husky commit

--- src/screens/other/ui-kit/ui-kit.tsx
+++ src/screens/other/ui-kit/ui-kit.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React from 'react'
+import { SafeAreaView, Text } from 'react-native'
 
 export function UiKit() {
-  return (
-    <SafeAreaView
-      style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
-      <Text
-        style={{
-          fontSize: 28,
-          fontWeight: '600',
-        }}>
-        Hello World
-      </Text>
-    </SafeAreaView>
-  );
+	return (
+		<SafeAreaView
+			style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}
+		>
+			<Text
+				style={{
+					fontSize: 28,
+					fontWeight: '600',
+				}}
+			>
+				Hello World
+			</Text>
+		</SafeAreaView>
+	)
 }
